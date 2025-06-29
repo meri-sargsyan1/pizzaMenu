4b42e3616b2a0a0db0c2d510fdebca832088b8d8
meri-sargsyan1 <meri.sargsyan.08@mail.ru>
2025-06-29 16:23:44 +0400

    popoxutyun3

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,25 +17,7 @@
          burgerIcon.src = "assets/images/burgerMenu.svg"; // ✅ բուրգեր նշանը
      }
  }
- // let menuOpen = false;
 
- // function toggleMenu() {
- //     const sideMenu = document.getElementById("sideMenu");
- //     const overlay = document.getElementById("overlay");
- //     const burgerIcon = document.getElementById("burgerIcon");
-
- //     menuOpen = !menuOpen;
-
- //     if (menuOpen) {
- //         sideMenu.classList.add("open");
- //         overlay.style.display = "block";
- //         burgerIcon.src = "assets/images/MENU.svg"; // ✅ Ֆիգմայից քո ներբեռնած X-ը
- //     } else {
- //         sideMenu.classList.remove("open");
- //         overlay.style.display = "none";
- //         burgerIcon.src = "assets/images/burgerMenu.svg"; // ✅ Ֆիգմայից քո բուրգերը
- //     }
- // }
  window.addEventListener('DOMContentLoaded', () => {
      const cityDiv = document.getElementById("city-select");
      const popup = document.getElementById("city-popup");
@@ -54,5 +36,40 @@
          });
      } else {
          console.error("Մի բան չի գտնվել։ Ստուգիր id-ները HTML-ում։");
+     }
+ });
+ window.addEventListener("DOMContentLoaded", () => {
+     const slider = document.getElementById("slider");
+     const scrollLeftBtn = document.getElementById("scrollLeft");
+     const scrollRightBtn = document.getElementById("scrollRight");
+
+     scrollLeftBtn.addEventListener("click", () => {
+         slider.scrollBy({ left: -300, behavior: "smooth" });
+     });
+
+     scrollRightBtn.addEventListener("click", () => {
+         slider.scrollBy({ left: 300, behavior: "smooth" });
+     });
+ });
+
+ //  --------------------top places---
+ const swiper = new swiper(".interesting-swiper", {
+     slidesPerView: 1.1,
+     spaceBetween: 20,
+     grabCursor: true,
+     pagination: {
+         el: ".swiper-pagination",
+         clickable: true,
+     },
+     breakpoints: {
+         600: {
+             slidesPerView: 1.5,
+         },
+         768: {
+             slidesPerView: 2,
+         },
+         1024: {
+             slidesPerView: 2.5,
+         }
      }
  });
